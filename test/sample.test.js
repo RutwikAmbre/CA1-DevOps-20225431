a2a3690bf66b582d266898aee8040580ad2b2592
RutwikAmbre <x20225431@student.ncirl.ie>
2025-03-11 14:28:43 +0000

    seconnd test run

---
+++ test/sample.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Math Tests', function () {
+    it('should add numbers correctly', function () {
+        assert.strictEqual(2 + 3, 5);
+    });
+
+    it('should subtract numbers correctly', function () {
+        assert.strictEqual(10 - 5, 5);
+    });
+});
