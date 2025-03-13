7f928a61e951dd2227ad51a70d053d96505b83ef
RutwikAmbre <x20225431@student.ncirl.ie>
2025-03-13 12:52:33 +0000

    push for auth synk

--- deployApp.sh
+++ deployApp.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-sudo apt update && sudo apt install nodejs npm
 
+# Update package lists and install Node.js and npm
+sudo apt update && sudo apt install nodejs npm -y
+
+# Install PM2 globally to manage Node.js applications
 sudo npm install -g pm2
 
+# Stop the existing application process (if running)
 pm2 stop simple_app
 
+# Navigate to the project directory
 cd CA1-DevOps-20225431/
 
+# Install project dependencies from package.json
 npm install
 
+# Save the private key and server certificate from environment variables to files
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
+# Start the application using PM2 with the specified entry point and environment
 pm2 start ./bin/www --name simple_app --env=production
